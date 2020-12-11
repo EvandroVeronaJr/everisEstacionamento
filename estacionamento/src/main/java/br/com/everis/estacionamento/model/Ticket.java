18cda77f4cda55314ba1e854846a96d7f52e0f21
Evandro Verona Jr <verona.junior@gmail.com>
2020-12-11 14:10:29 -0300

    correção de erros, reiniciei tudo

---
+++ estacionamento/src/main/java/br/com/everis/estacionamento/model/Ticket.java
@@ -0,0 +1,8 @@
+package br.com.everis.estacionamento.model;
+
+import javax.persistence.Entity;
+
+//@Entity	
+public class Ticket {
+
+}
--- estacionamento/src/main/java/br/com/everis/estacionamento/repository/TicketRepository.java
+++
@@ -1,7 +0,0 @@
-package br.com.everis.estacionamento.repository;
-import org.springframework.data.jpa.repository.JpaRepository;
-import br.com.everis.estacionamento.modelo.Ticket;
-
-public interface TicketRepository extends JpaRepository<Ticket,Long> {
-
-}
